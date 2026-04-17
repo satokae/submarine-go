8f2697ac3d2b8d9b981d22c2b41b45620189ade7
satokae <208755468+satoKae@users.noreply.github.com>
2026-04-17 12:07:44 +0900

    feat: GetNeighbors

--- internal/core/position.go
+++ internal/core/position.go
@@ -21,3 +21,21 @@ func (pos Position) Move(dx int, dy int) (Position, error) {
 	}
 	return Position(y*constant.MapSize + x), nil
 }
+
+func GetNeighbors(pos Position) []Position {
+	neighbors := []Position{}
+
+	for dx := -1; dx <= 1; dx++ {
+		for dy := -1; dy <= 1; dy++ {
+			if dx == 0 && dy == 0 {
+				continue
+			}
+
+			neighborPos, err := pos.Move(dx, dy)
+			if err == nil {
+				neighbors = append(neighbors, neighborPos)
+			}
+		}
+	}
+	return neighbors
+}
